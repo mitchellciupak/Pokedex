a15a3bc3b21fc61fd827de3093ee7218b1ea72ce
mitchellciupak <ciupak@purdue.edu>
2020-10-29 18:47:23 -0400

    build

---
+++ precache-manifest.06cdb4eb188d89e9ab85cf14bf2a5f56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3592d6553c75e0b345f9306a97dcf6a2",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "53b31fe0b6370ebf719f",
+    "url": "/Pokedex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0b6632a4504df861432f",
+    "url": "/Pokedex/static/js/2.e3e34cfa.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/Pokedex/static/js/2.e3e34cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b31fe0b6370ebf719f",
+    "url": "/Pokedex/static/js/main.8112bb6e.chunk.js"
+  },
+  {
+    "revision": "9a9198cabd1dd06d36e3",
+    "url": "/Pokedex/static/js/runtime-main.442a7f2b.js"
+  }
+]);
